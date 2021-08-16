e66c62ecf41f2cbade5891bb0f7fa8b3f441345e
Ibai Maquina Virtual <ibai3553@gmail.com>
2021-08-16 17:20:17 +0200

    Se crea una tabla azul automaticamente

--- js/index.js
+++ js/index.js
@@ -4,12 +4,26 @@ $(document).ready(function() {
     $("#crearBingo").click(function() {
         let zonaTexto = $("#zonaTexto").val()
         var arrayTexto = []
-
+        let html = ""
         saveText(zonaTexto).forEach(a => {
             arrayTexto.push(a)
         });
 
         console.log(arrayTexto)
+
+
+        html += "<table>"
+        for (let i = 0; i < 5; i++) {
+            html += "<tr></tr>"
+            for (let z = 0; z < 5; z++) {
+                html += "<th></th>"
+            }
+            html += "</tr>"
+
+        }
+        html += "</table>"
+        $("#Bingo").html(html)
+
     });
 
 });
