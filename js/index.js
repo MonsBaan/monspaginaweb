81888d0b213a34eb18def4afee807fc2160140ed
Ibai Maquina Virtual <ibai3553@gmail.com>
2021-08-16 17:36:47 +0200

    SHUFFLE

--- js/index.js
+++ js/index.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
         let zonaTexto = $("#zonaTexto").val()
         var arrayTexto = []
         let html = ""
+        let cont = 0
         saveText(zonaTexto).forEach(a => {
             arrayTexto.push(a)
         });
 
-        console.log(arrayTexto)
-
-
+        arrayFinal = shuffle(arrayTexto);
         html += "<table>"
         for (let i = 0; i < 5; i++) {
             html += "<tr></tr>"
             for (let z = 0; z < 5; z++) {
-                html += "<th></th>"
+                html += "<th>" + arrayFinal[cont] + "</th>"
+                cont++
             }
             html += "</tr>"
 
@@ -45,4 +45,26 @@ function saveText(texto) {
         }
     }
     return arrayTemp;
+}
+
+function shuffle(array) {
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+    var currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
+    }
+
+    return array;
 }
